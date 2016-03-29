a40c8ddd9e51187ca107e04ef8f3db992ce39a23
Iris Schaffer <irisSchaffer@gmx.net>
2016-03-30 00:07:42 +0200

    Adds dope path branches

--- client/index.js
+++ client/index.js
@@ -4,19 +4,39 @@ import { PrismCode } from "react-prism";
 
 import { UnveilApp, Slide, Notes, KeyControls, UIControls, TouchControls, Presenter } from '../../unveil.js/src';
 import { SocketReceiver, SocketSender } from '../../unveil-network-sync/src';
-import { MediaSender, MediaReceiver, MediaAcceptor, SpeakerPresenter, Voting, Question, Answer, VotingController } from '../../unveil-interactive/src';
+import {
+  SpeakerPresenter,
+  Redirect, Link,
+  Voting, Question, Answer,
+  VotingController, VotingReceiver, VotingCreator, VotingNavigatableSetter,
+  MediaSender, MediaReceiver, MediaAcceptor
+} from '../../unveil-interactive/src';
 
 let modes = {
   default: {
-    controls : [KeyControls, TouchControls, UIControls, SocketReceiver, MediaSender, MediaReceiver, VotingController],
+    controls : [
+      KeyControls, TouchControls, UIControls,
+      SocketReceiver,
+      MediaSender, MediaReceiver,
+      VotingNavigatableSetter, VotingReceiver
+    ],
     presenter: Presenter
   },
   speaker: {
-    controls : [KeyControls, TouchControls, MediaAcceptor, MediaReceiver, SocketSender, SocketReceiver, VotingController],
+    controls : [
+      KeyControls, TouchControls,
+      MediaAcceptor, MediaReceiver,
+      SocketSender, SocketReceiver,
+      VotingController, VotingCreator, VotingReceiver
+    ],
     presenter: SpeakerPresenter
   },
   projector: {
-    controls : [SocketReceiver, MediaReceiver, VotingController],
+    controls : [
+      SocketReceiver,
+      MediaReceiver,
+      VotingNavigatableSetter, VotingReceiver
+    ],
     presenter: Presenter
   }
 };
@@ -24,14 +44,6 @@ let modes = {
 ReactDOM.render( (
 <section>
   <UnveilApp modes={modes}>
-    <Slide name="voting">
-      <Voting name="like-slides">
-        <Question>How do you like these slides?</Question>
-        <Answer value="muchito">A whole lot!</Answer>
-        <Answer value="a-bito">So lala...</Answer>
-        <Answer value="hatito">Booo!</Answer>
-      </Voting>
-    </Slide>
     <Slide name="start">
       <h1>Unveil</h1>
       <h2>a meta presentation</h2>
@@ -70,7 +82,6 @@ ReactDOM.render( (
         remote control, polling, feeding...
       </Notes>
     </Slide>
-
     <Slide name="science">
       <h1>Relevant Research...</h1>
       <Notes>
@@ -102,32 +113,74 @@ ReactDOM.render( (
       <h1>Explore...</h1>
       <Notes>
         I wanted to explore the potential of mobile devices<br/>
-
       </Notes>
+    </Slide>
+
+    <Slide name="behind-the-scenes">
+      <h1>Behind the Scenes</h1>
+    </Slide>
+    <Slide name="technology-voting">
+      <Voting name="technology">
+        <Question>How much do you want to hear about the technologies involved?</Question>
+        <Answer value="details">Details, including code examples</Answer>
+        <Answer value="overview">Short Overview of general architecture</Answer>
+        <Answer value="noting">Skip It</Answer>
+      </Voting>
+    </Slide>
+    <Slide name="technology-paths">
+      <h2>How much do you want to hear about the technologies involved?</h2>
+      <ul>
+        <li><Link target="/reveal">Details</Link></li>
+        <li><Link target="/technology-overview">Short Overview</Link></li>
+        <li><Link target="/future">Skip It</Link></li>
+      </ul>
+    </Slide>
+    <Slide name="technology-overview">
+      <img src="./img/architecture.png" />
+      <Notes>
+        Unveil.js: Core library, developed with friend. Takes care of displaying general presentation and slides, navigation between them etc.<br />
+        Network: Network Synchronisation layer to send location changes to server and receive them<br />
+        Interactive: Media creation/acceptation/reception, votes, etc.<br />
+        Server: Server for communication between devices, serves actual presentation
+      </Notes>
+    </Slide>
+    <Slide name="technology-overview-implementation" right={[23]}>
+      <h1>Implementation</h1>
+      <ul>
+        <li><strong>Components</strong> – What should the slides look like?</li>
+        <li><strong>Controls</strong> – How can we interact with the presentation?</li>
+        <li><strong>Presenter</strong> – How should slides and controls be rendered?</li>
+      </ul>
     </Slide>
     <Slide name="reveal">
       <h1>reveal.js</h1>
       <Notes>
-        revealjs is a 4500+ line lib with plugins <br/>
-        no separation of rendering, navigation, routing...<br/>
-        plugins messed up (several servers etc.)<br/><br/>
-        developed base lib with friend<br/>
-        adjusted to my needs + added my features
+        The mission: Find extensible presentation library <br />
+        revealjs is a 4500+ line lib with plugins <br />
+        no separation of rendering, navigation, routing...<br />
+        plugins messed up (several servers etc.)<br /><br />
       </Notes>
     </Slide>
-    <Slide name="behind-the-scenes">
-      <h1>Behind the Scenes</h1>
+    <Slide name="architecture">
+      <img src="./img/architecture.png" />
+      <Notes>
+        Unveil.js: Core library, developed with friend. Takes care of displaying general presentation and slides, navigation between them etc.<br />
+        Network: Network Synchronisation layer to send location changes to server and receive them<br />
+        Interactive: Media creation/acceptation/reception, votes, etc.<br />
+        Server: Server for communication between devices, serves actual presentation
+      </Notes>
     </Slide>
     <Slide name="technologies">
       <img src="./img/rxjs-logo.png" />
       <img src="./img/react-logo.png" />
       <img src="./img/socketio-logo.gif" />
     </Slide>
-    <Slide name="architecture">
-      <h1>Architecture</h1>
+    <Slide name="implementation">
+      <h1>Implementation</h1>
       <ul>
-        <li><strong>Presenter</strong> – How should slides be rendered?</li>
-        <li><strong>Controls</strong> – How should slides interact?</li>
+        <li><strong>Components</strong> – What should the slides look like?</li>
+        <li><strong>Controls</strong> – How can we interact with the presentation?</li>
+        <li><strong>Presenter</strong> – How should slides and controls be rendered?</li>
       </ul>
     </Slide>
     <Slide name="modes">
